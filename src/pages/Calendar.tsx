26d2ac0217a7cb4a716e086dc0f41d0ac6bb234e
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-21 14:53:57 +0900

    ➕ 캘린더 라이브러리 설치

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
-function Calendar() {
+function Page() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange =  (date: Date, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setSelectedDate(date);
+  };
   return (
-    <Container>
-      <h1>Calendar 페이지</h1>
-      {/* 여기에 Calendar 페이지의 내용을 추가하세요 */}
+    <Container className="mt-4">
+      <Row>
+        <Col>
+          <h2>일정 관리 페이지</h2>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col md={8}>
+          <Calendar
+            onChange={handleDateChange as any}
+            value={selectedDate}
+            locale="ko-KR"
+            calendarType="gregory"
+            //style css 오버라이딩은 node_modules > react-calendar > dist > Calendar.css
+          />
+        </Col>
+        <Col md={4}>
+          <h3>선택된 날짜: {selectedDate.toDateString()}</h3>
+          {/* 이 곳에 선택된 날짜의 일정 정보를 표시할 컴포넌트 또는 기능을 추가할 수 있습니다 */}
+          <Button variant="primary" className="mt-2">새 일정 추가</Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
 
-export default Calendar;
+export default Page;
