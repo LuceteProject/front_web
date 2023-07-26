5527a0a74ab7bf07f5dd373483e035a7900cf864
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-26 15:49:38 +0900

    🎨 투두리스트 페이지 구성 추가

--- src/pages/Todolist.tsx
+++ src/pages/Todolist.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from "react";
+
+import "../styles/Todo.css"; // Board.css 파일에서 추가적인 스타일을 정의합니다.
+
+import { Container, Form } from "react-bootstrap";
+
+interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+  type: string;
+}
 
 function Page() {
   return (
-    <Container>
-      <h1>Todolist 페이지</h1>
-      {/* 여기에 Todolist 페이지의 내용을 추가하세요 */}
+    <Container className="mt-4">
+      <p> To do page</p>
+      <div className="check-circle" />
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formAll">
+          <Form.Label>전체</Form.Label>
+          <Form.Control type="text" placeholder="항목을 입력하세요." />
+          <div className="check-circle" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formTeam">
+          <Form.Label>팀</Form.Label>
+          <Form.Control type="text" placeholder="항목을 입력하세요." />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formIndividual">
+          <Form.Label>개인</Form.Label>
+          <Form.Control type="text" placeholder="항목을 입력하세요." />
+        </Form.Group>
+      </Form>
     </Container>
   );
 }
-
 export default Page;
