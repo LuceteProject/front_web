8a948bbb93afe8c34779652d422d995e2a1a4590
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-27 11:03:23 +0900

    🎨 Todo, Post 타입 생성

---
+++ src/types.tsx
@@ -0,0 +1,16 @@
+export type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+  category: string;
+  check: boolean;
+};
+
+export type Post = {
+  id: number;
+  title: string;
+  author_name: string;
+  updated: string;
+  content: string;
+  permission: number;
+};
