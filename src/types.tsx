f6c485ace4327939fb5491cecc72d3268c10a006
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-07-31 14:47:04 +0900

    💄 CSS 스타일 지정, type 추가

--- src/types.tsx
+++ src/types.tsx
@@ -9,18 +9,29 @@ export type TodoCategory = "All" | "Team" | "Personal";
 
 export type Post = {
   id: number;
+  header: number;
   title: string;
+  author_id: number;
   author_name: string;
   updated: string;
   content: string;
   permission: number;
+  is_notice: false;
+  board_id: 0;
+};
+
+export type Reply = {
+  id: number;
+  author_name: string;
+  content: string;
+  created: string;
 };
 
 export type User = {
-  id : any;
-  name : string; // 이름
-  team : string; // 소속
-  message : string; //profile message
-  number : number; //기수(논의필요)
-  image : string; //profile image(논의필요)
-}
+  id: any;
+  name: string; // 이름
+  team: string; // 소속
+  message: string; //profile message
+  number: number; //기수(논의필요)
+  image: string; //profile image(논의필요)
+};
