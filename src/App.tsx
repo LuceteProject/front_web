6e7600b647d3343d3a7fe8db309e8dd508ae437e
정하리 <50551231+jha2ee@users.noreply.github.com>
2023-08-17 12:49:02 +0900

    ✨ 로그인 유무에 따라 접근제어

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -21,47 +21,93 @@ import BoardPost from "./pages/BoardPost";
 import { Button } from "react-bootstrap";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleNaverLogin = (response: any) => {
+    if (response) {
+      // 로그인 성공한 경우
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <Router>
       <Header />
       <div className="page-wrapper">
-        <Footer />
-        <Routes>
-          {/* URL 형식 : http://localhost:3000/board */}
-          <Route path="/" Component={Home} />
-          <Route path="/board" Component={Board} />
-          <Route path="/todolist" Component={Todolist} />
-          <Route path="/calendar" Component={Calendar} />
-          <Route path="/drive" Component={Drive} />
-          <Route path="/profile" Component={Profile} />
+        {isLoggedIn ? (
+          <>
+            <Footer />
+            <Routes>
+              {/* URL 형식 : http://localhost:3000/board */}
+              <Route path="/" Component={Home} />
+              <Route path="/board" Component={Board} />
+              <Route path="/todolist" Component={Todolist} />
+              <Route path="/calendar" Component={Calendar} />
+              <Route path="/drive" Component={Drive} />
+              <Route path="/profile" Component={Profile} />
 
-          <Route path="/board/:postId" Component={BoardPost} />
+              <Route path="/board/:postId" Component={BoardPost} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </>
+        ) : (
+          <>
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </>
+        )}
       </div>
     </Router>
   );
-}
 
+  function Home() {
+    return (
+      <>
+        {isLoggedIn ? (
+          <div className="container">
+            <div className="text-center mt-5">
+              <h1 style={{ marginBottom: 50 }}>Welcome to Lucete!</h1>
+              <p>로그아웃하려면 아래 버튼을 누르세요.</p>
+              <Button className="custom-button" onClick={redirectToExternalURL}>
+                로그아웃
+              </Button>
+              <div />
+              {/* 
+          <Button className="custom-button" disabled={!isLoggedIn}>
+            로그아웃
+          </Button>
+          */}
+            </div>
+          </div>
+        ) : (
+          <div className="container">
+            <div className="text-center mt-5">
+              <h1 style={{ marginBottom: 50 }}>Welcome to Lucete!</h1>
+              <p>로그인하려면 아래 버튼을 누르세요.</p>
+              <p>네이버 아이디가 필요합니다.</p>
+              <Button className="custom-button" onClick={redirectToExternalURL}>
+                로그인
+              </Button>
+              <div />
+              {/* 
+          <Button className="custom-button" disabled={!isLoggedIn}>
+            로그아웃
+          </Button>
+          */}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
+}
 // http://54.237.121.196:8080/oauth2/authorization/naver
 const redirectToExternalURL = () => {
-  window.location.href = "http://54.237.121.196:8080/oauth2/authorization/naver";
+  window.location.href =
+    "http://54.237.121.196:8080/oauth2/authorization/naver";
   return null;
 };
-function Home() {
-  return (
-    <div className="container">
-      <div className="text-center mt-5">
-        <h1 style={{marginBottom: 50}}>Welcome to Lucete!</h1>
-        <p>로그인하려면 아래 버튼을 누르세요.</p>
-        <p>네이버 아이디가 필요합니다.</p>
-        <Button className="custom-button" onClick={redirectToExternalURL}>
-          로그인 
-        </Button>
-      </div>
-    </div>
-  );
-}
-
 export default App;
